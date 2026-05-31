bec08f4cc80e634719902535e868039fe3d5251f
agnieszkasankowska9 <agnieszkasankowska9@gmail.com>
2026-05-31 13:42:29 +0000

    update workflow with cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "nswvvw",
+  projectId: "xu4m4s",
   allowCypressEnv: false,
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
